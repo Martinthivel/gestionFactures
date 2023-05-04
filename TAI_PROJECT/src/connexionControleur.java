c77e51e3efd8f1a24077565c99527e1600fefbc8
bmousques <baptiste.mousques@etu.estia.fr>
2023-05-04 16:39:08 +0200

    Redirection vers la page appropriée suivant l'emplpoye qui se connecte

--- TAI_PROJECT/src/connexionControleur.java
+++ TAI_PROJECT/src/connexionControleur.java
@@ -72,12 +72,42 @@ public class connexionControleur extends HttpServlet {
         session.setAttribute("employe", employe);
 
         // Rediriger l'utilisateur vers la page appropriée
-        try {
+       /* try {
 			response.sendRedirect("clientVue.jsp");// ici on met la page vers laquelle on redirige
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+		}*/
+        String role = employe.getRole();
+        if (role.equals("Préparateur de commandes")) {
+            try {
+				response.sendRedirect("expeditionVue.jsp");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        } else if (role.equals("Gestionnaire des ventes")) {
+            try {
+				response.sendRedirect("financeVue.jsp");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        } else if (role.equals("Commercial des ventes")) {
+            try {
+				response.sendRedirect("commandeVue.jsp");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        } else {
+            try {
+				response.sendRedirect("clientVue.jsp");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        }
       } else {
         // Afficher un message d'erreur si les données de connexion sont invalides
         request.setAttribute("messageErreur", "Nom d'utilisateur ou mot de passe incorrect.");
